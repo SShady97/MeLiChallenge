c65e316437876923151628e4ae93ed390ba32359
SShady97 <joseez182@gmail.com>
2022-09-27 12:19:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from functions.date import setToLocalTime
 class TestSetLocalTime(unittest.TestCase):
     def test_set_local_time(self):
         self.assertEqual(setToLocalTime("2022-09-25T03:48:51.913Z"), "2022-09-25 00:48")
-        self.assertEqual(setToLocalTime("2022-03-12T05:54:22.543Z"), "2022-03-12 03:54")
+        self.assertEqual(setToLocalTime("2022-03-12T05:54:22.543Z"), "2022-03-12 02:54")
 
 if __name__ == '__main__':
     unittest.main()
